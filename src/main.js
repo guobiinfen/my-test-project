cec2adfd49d7805dfd3f253de87f8fb0e085b3a0
Your Name <you@example.com>
2019-08-24 13:09:57 +0800

    第二次提交

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from "./router";
 import store from "./store";
 import { Button, Select, Table,TableColumn,Checkbox ,CheckboxGroup} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.config.productionTip = false;
 Vue.use(Button)
 Vue.use(Select)
 Vue.use(Table)
